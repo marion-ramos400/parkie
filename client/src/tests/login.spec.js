51352a28a0452446900bdf1539008c087fb9f20d
mram400 <marion.ramos400@gmail.com>
2025-09-30 00:17:48 +0800

    removed comment

--- client/src/tests/login.spec.js
+++ client/src/tests/login.spec.js
@@ -10,16 +10,11 @@ import {
 import Login from '../components/Login/Login.vue'
 
 
-test('has form', () => {
+test('form exists', () => {
   const wrapper = mount(Login)
   const form = wrapper.find('form')
   expect(form.exists()).toBe(true)
   expect(form.find('input').exists()).toBe(true)
-//  expect(wrapper.html()).toContain('<form>');
 })
 
-//test('form has username and pwd input', () => {
-//  const elem = wrapper.find('form')
-//  console.log(typeof(elem))
-//})
 
